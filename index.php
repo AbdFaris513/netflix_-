10ffe413861f042d367f1c30785c688909cd9ded
AbdFaris513 <106486197+AbdFaris513@users.noreply.github.com>
2022-08-17 13:05:27 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Mouse+Memoirs&family=Pacifico&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Mouse+Memoirs&family=Pacifico&display=swap" rel="stylesheet">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Netflix</title>
+
+</head>
+<body class="bd">
+    <h1>Netflix</h1>
+    <div class="content">
+        <h3>Sign in</h3>
+        <form action="nakku.html" method="get">
+            <div class="email">
+                <input class="in" type="email" id="email" name="email" placeholder="Email or phnone number" required>
+            </div>
+            <div class="password">
+                <input class="in" type="password" id="email" name="email" placeholder="Password" minlength="8" required>
+            </div>
+            <div class="check">
+                <input type="checkbox" id="cb" name="cb">
+                <label for="cb">Remember me</label>
+                <a class="a1" href="">Need help?</a>
+            </div>
+            <div>
+                
+            </div>
+            <div class="btn">
+                <button type="submit">
+                    Sign in
+                </button>
+            </div>
+            <div class="discr">
+                <p class="p1">New to Netflix?
+                     <a class="singup" href=""> Sign up now.</a> </p>
+                <p>This page is protected by Google
+                     reCAPTCHA to ensure you're not a bot. 
+                     <a class="lasta">Learn more.</a></p> 
+                     <h4 class="abd">Abd</h4>
+                        <h4 class="faris"> Faris.</h4>
+            </div>
+        </form>
+        
+    </div>
+</body>
+</html>
